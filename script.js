af065c5612e3474a44331a5006cdb12e2b26ea7d
Arasamannar A K <arasamannar@gmail.com>
2023-10-16 19:29:21 +0530

    added related files for the task.

---
+++ script.js
@@ -0,0 +1,13 @@
+function countdownCallback(number) {
+    // Display the number on the screen.
+    document.getElementById("countdown").innerHTML = number;
+  
+    // If the number is 1, display "Happy Independence Day!" and stop the countdown.
+    if (number === 1) {
+      document.getElementById("countdown").innerHTML = "Happy Independence Day!";
+      return;
+    }
+  
+    // Schedule the next callback function to be executed in 1 second.
+    setTimeout(countdownCallback.bind(null, number - 1), 1000);
+  }
